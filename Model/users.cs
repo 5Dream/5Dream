4aa9f49f07e7a3172c1a688b828ef38e09385131
25444 <25444@DESKTOP-R484EE7>
2016-10-07 16:59:28 +0800

    数据访问层代码和模型层的代码

---
+++ Model/users.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Model
+{
+  public class TabTeachers
+    {
+        private string Department;
+        private string UserID;
+        private string USerPWD;
+        private string UserName;
+        private string Role;
+        public string _Deparetment
+        {
+            get { return Department;}
+            set { Department = value;}
+
+        }
+        public string _UserID
+        {
+            get { return UserID;}
+            set { UserID = value;}
+
+        }
+        public string _UserPWD
+        {
+            get { return USerPWD; }
+            set { USerPWD = value; }
+        }
+        public string _UserName
+        {
+            get { return UserName; }
+            set { UserName = value;}
+        }
+        public string _Role
+        {
+            get { return Role; }
+            set { Role = value; }
+        }
+    }
+}
